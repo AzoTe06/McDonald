914a703addee966de7440fe0b7d4d9b9c077140b
AzoTe06 <nixodu06@gmail.com>
2017-06-15 22:19:53 +0200

    Add files via upload

---
+++ mc_do/SQL pour les 3 BONUS.sql
@@ -0,0 +1,8 @@
+INSERT INTO `items` (`id`, `libelle`, `isIllegal`, `value`, `type`) VALUES
+(26, 'Sandwich', '0', 30, 2),
+(30, 'Hamburger', '0', 1, 2),
+(25, 'Frites', '0', 2, 2),
+(31, 'Eau', '0', 45, 1),
+(33, 'Café', '0', 10, 1),
+(34, 'Barre Choco', '0', 20, 2),
+(35, 'Coca zero', '0', 3, 1);
